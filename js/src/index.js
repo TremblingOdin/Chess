b0a24dc08d575c6b8391fa1dd5bf702b2cb91ca7
TremblingOdin <pigmekevin@gmail.com>
2020-10-08 18:17:12 -0600

    started basic logic

---
+++ js/src/index.js
@@ -0,0 +1,12 @@
+class Chessboard {
+	constructor(elementid, cpu, players) {
+		document.getElementById(elementid);
+		if(cpu != false) {
+			this.player2 = cpu;
+			this.player1 = players.player1;
+		} else {
+			this.player1 = players.player1;
+			this.player2 = players.player2;
+		}
+	}
+}
