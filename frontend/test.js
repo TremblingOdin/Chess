4d7900fcdbe1192e6cf3a69841f953654b0d7345
TremblingOdin <pigmekevin@gmail.com>
2020-10-09 16:56:10 -0600

    setup chessboard initialization

--- frontend/test.js
+++ frontend/test.js
@@ -1,6 +1,9 @@
 import Chessboard from './index/chessboard.js";
 
 window.onload = () => {
-	var chessdiv = document.getElementById("chessboard");
-	
+	var players = new Object();
+	players.player1 = true;
+	players.player2 = true;
+
+	const chessboard = new Chessboard("chessboard", false, players);
 };
