bea20d288dccfbd6597b606515a816fca2516533
TremblingOdin <pigmekevin@gmail.com>
2020-10-13 19:43:15 -0600

    created basic data access function in main

--- backend/python/src/main.py
+++ backend/python/src/main.py
@@ -4,3 +4,15 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
+
+
+normalized_csv = 'chess_normalized.csv'
+
+def access_data(data_dir):
+    os.chdir(data_dir)
+    df = pd.read_csv(normalized_csv)
+    data = df['moves'].tolist()[:500]
+    split_data = []
+    indice = 500
+
+
