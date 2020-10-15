4373239a0704ebc21126e32a4008c33e78f53e5e
TremblingOdin <pigmekevin@gmail.com>
2020-10-15 14:30:10 -0600

    Renamed montecarlotreesearch and added leaf check

--- backend/python/src/mcts.py
+++ backend/python/src/mcts.py
@@ -6,3 +6,8 @@ class Node():
         self.playerTurn = state.playerTurn
         self.id = state.id
         self.edges = [] # This is a graph so edges are how this node connnects to other nodes
+
+    def is_leaf(self):
+        if len(self.edges) > 0:
+            return False
+        return True
